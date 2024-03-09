ec0b16665be3ad05477c252cb3c69dd514c4e3d2
Andreas-206 <puma_real@ukr.net>
2024-03-09 10:07:40 +0200

    Update MovieCast.jsx

--- src/components/MovieCast.jsx
+++ src/components/MovieCast.jsx
@@ -1,4 +1,20 @@
-const { movieId } = useParams()
-useEffect(() => {
-	if (!movieId) return
-}, [movieId])
+import { useParams, useEffect } from "react-router-dom"
+import React from 'react'
+
+function MovieCast() {
+	const { movieId } = useParams()
+	useEffect(() => {
+		if (!movieId) return
+	}, [movieId]);
+
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default MovieCast
+
+
+
