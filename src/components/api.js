06b6fd6083e39d7899cece3d6173bdde6bcba839
Andreas-206 <puma_real@ukr.net>
2024-03-11 16:18:10 +0200

    url

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,37 @@
-import axios from "axios";
+import axios from 'axios'
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3'
 
-export default async function () {
-  
+const options = {
+	headers: {
+		Authorization:
+			'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzNiMzQ1YTQ2NDk3MDFmNmE1OWMwOGUzYmI1ZDg0MyIsInN1YiI6IjY1ZWIxYWUxN2Y0ZjIxMDE3YmRlMDE3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.snRIexDM-X47F19MFalBoGhIrtUry-B6hIKZwh4O-t0',
+	},
 }
+
+export async function fetchMovies() {
+	const { data } = await axios.get('/trending/movie/day', options)
+	return data.results
+}
+
+export async function fetchMovie(id) {
+	const { data } = await axios.get(`/movie/${id}`, options)
+	return data
+}
+
+export async function fetchMovieCredits(id) {
+	const { data } = await axios.get(`/movie/${id}/credits`, options)
+	return data.cast
+}
+
+export async function fetchMovieReview(id) {
+	const { data } = await axios.get(`/movie/${id}/reviews`, options)
+	return data.results
+}
+
+export async function fetchMoviesByTitle(search) {
+	const { data } = await axios.get(`/search/movie?query=${search}`, options)
+	return data.results
+}
+
+export default async function () {}
