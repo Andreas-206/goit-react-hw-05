51cf2a23b328e5d49425b4a003c2289e83e21a3a
Andreas-206 <puma_real@ukr.net>
2024-03-11 18:13:31 +0200

    +

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+import { fetchMovies } from '../components/api'
+
 export default function HomePage() {
+	const [movies, setMovies] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(false)
+
+	useEffect(() => {
+		async function getData() {
+			try {
+				setLoading(true)
+				const data = await fetchMovies()
+				setMovies(data.results)
+			} catch (error) {
+				setError(true)
+			} finally {
+				setLoading(false)
+			}
+		}
+		getData()
+	}, [])
+
 	return (
-		<div>
+		<>
 			<h1>Trending today</h1>
-		</div>
+			{loading && <Loader />}
+			<div></div>
+		</>
 	)
 }
